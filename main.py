2ab24941b47c9a67c71b9b058f711b13de40481a
shengnan0218 <150988149+shengnan0218@users.noreply.github.com>
2023-11-24 20:54:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,41 @@
-from flask import Flask, request, jsonify, redirect, session, url_for
-from google_auth_oauthlib.flow import Flow
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-import google.auth
+import os
 
-app = Flask(__name__)
-app.secret_key = 'AIzaSyCRBfdXfpkBwiTAvJlrVcCBNEa0wAJYMhI'  # Set a secret key for session management
+app = FastAPI()
 
-# Google OAuth 2.0 Client Configuration
-# Replace with your client_id.json file path
-CLIENT_SECRETS_FILE = "client_id.json"
+# 这是您的OAuth 2.0客户端配置文件路径
+CLIENT_SECRETS_FILE = 'path/to/your/client_secret.json'
+
+# 定义所需的API权限范围
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-@app.route('/')
-def index():
-    # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow steps
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES)
-    flow.redirect_uri = url_for('oauth2callback', _external=True)
+# 请求模型
+class FolderContentRequest(BaseModel):
+    folder_id: str
 
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        include_granted_scopes='true')
+def get_google_drive_service():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server(port=0)
+    return build('drive', 'v3', credentials=credentials)
 
-    # Store the state in session for later use
-    session['state'] = state
-
-    return redirect(authorization_url)
-
-@app.route('/oauth2callback')
-def oauth2callback():
-    # Specify the state when creating the flow in the callback so that it can be verified in the authorization server response
-    state = session['state']
-
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    flow.redirect_uri = url_for('oauth2callback', _external=True)
-
-    # Use the authorization server's response to fetch the OAuth 2.0 tokens
-    authorization_response = request.url
-    flow.fetch_token(authorization_response=authorization_response)
-
-    # Store credentials in the session
-    # Credentials are available in flow.credentials
-    session['credentials'] = {
-        'token': flow.credentials.token,
-        'refresh_token': flow.credentials.refresh_token,
-        'token_uri': flow.credentials.token_uri,
-        'client_id': flow.credentials.client_id,
-        'client_secret': flow.credentials.client_secret,
-        'scopes': flow.credentials.scopes
-    }
-
-    return redirect(url_for('test_api'))
-
-@app.route('/files/<fileId>', methods=['GET'])
-def get_file(fileId):
-    if 'credentials' not in session:
-        return redirect('authorize')
-
-    # Load credentials from the session
-    credentials = google.oauth2.credentials.Credentials(
-        **session['credentials'])
-
+@app.post("/get-folder-content/")
+def get_folder_content(request: FolderContentRequest, service=Depends(get_google_drive_service)):
     try:
-        # Build the Google Drive service
-        service = build('drive', 'v3', credentials=credentials)
-
-        # Call the Drive API
-        file = service.files().get(fileId=fileId).execute()
-
-        return jsonify(file)
-
+        # 在这里使用service对象调用Google Drive API
+        folder_id = request.folder_id
+        # 示例: 列出文件夹中的文件
+        response = service.files().list(q=f"'{folder_id}' in parents").execute()
+        return response.get('files', [])
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
